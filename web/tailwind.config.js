0a11807bb7a644f3d35d7f6e501d89c2cf8112fe
Joakim Stenberg <joakim.stenberg88@gmail.com>
2022-06-02 15:19:32 +0200

    cleannup of unused packages

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [
-      require('flowbite/plugin')
   ]
 }
